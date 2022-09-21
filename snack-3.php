51b3f28fb0523f030946d074d974c87c18ecf7f8
Alaikum <carangiadamo@gmail.com>
2022-09-21 20:15:12 +0200

    Snack3 corretto

--- snack-3.php
+++ snack-3.php
@@ -11,36 +11,36 @@ $posts = [
     '10/01/2022' => [
         [
             'title' => 'Post 1',
-            'author' => 'Michele Papagni',
+            'author' => 'Michele Papagni1',
             'text' => 'Testo post 1'
         ],
         [
             'title' => 'Post 2',
-            'author' => 'Michele Papagni',
+            'author' => 'Michele Papagni2',
             'text' => 'Testo post 2'
         ],
     ],
     '10/02/2022' => [
         [
             'title' => 'Post 3',
-            'author' => 'Michele Papagni',
+            'author' => 'Michele Papagni3',
             'text' => 'Testo post 3'
         ]
     ],
     '15/05/2022' => [
         [
             'title' => 'Post 4',
-            'author' => 'Michele Papagni',
+            'author' => 'Michele Papagni4',
             'text' => 'Testo post 4'
         ],
         [
             'title' => 'Post 5',
-            'author' => 'Michele Papagni',
+            'author' => 'Michele Papagni5',
             'text' => 'Testo post 5'
         ],
         [
             'title' => 'Post 6',
-            'author' => 'Michele Papagni',
+            'author' => 'Michele Papagni6',
             'text' => 'Testo post 6'
         ]
     ],
@@ -49,45 +49,37 @@ $posts = [
 // var_dump( $posts[ '10/01/2022'][0]['title']);
 
 
-//     var_dump($posts['10/01/2022']);
+// var_dump(array_keys($posts));
+
 
 
 
 ?>
 
 
-<ul> Post del 10/01/2022
+<? for ($i = 0; $i < count($posts); $i++) {
 
-    <?
-    $post_10 = $posts['10/01/2022'];
-    for ($i = 0; $i < count($post_10); $i++) {
-        $post_10_stampa = $post_10[$i]
+    $name = (array_keys($posts));
+    // echo $name[$i];
+    // var_dump($posts[$name[$i]]);
+    $date = $name[$i];
+    $postPerData = ($posts[$date]);
+    // var_dump($postPerData);
     ?>
-        <li> <?= " Titolo: {$post_10_stampa['title']} , autore: {$post_10_stampa['author']} e testo : {$post_10_stampa['text']} .  " ?> </li>
-    <? }
-    ?>
-</ul>
 
-<ul> Post del 10/02/2022
+    <ul> Post del: <?= $date;
+       
+        for ($j = 0; $j < count($postPerData); $j++) {
+            $postTitle = $postPerData[$j]['title'];
+            $postAuthor = $postPerData[$j]['author'];
+            $postText = $postPerData[$j]['text'];
 
-    <?
-    $post_10_02 = $posts['10/02/2022'];
-    for ($i = 0; $i < count($post_10_02); $i++) {
-        $post_10_02stampa = $post_10_02[$i]
-    ?>
-        <li> <?= " Titolo: {$post_10_02stampa['title']} , autore: {$post_10_02stampa['author']} e testo : {$post_10_02stampa['text']} .  " ?> </li>
-    <? }
-    ?>
-</ul>
+        ?> <li> <?= "Title: $postTitle, Autor: $postAuthor, Text: $postText" ?> </li>
+        <?
 
-<ul> Post del 15/05/2022
+        } ?>
 
-    <?
-    $post_15 = $posts['15/05/2022'];
-    for ($i = 0; $i < count($post_15); $i++) {
-        $post_15_stampa = $post_15[$i]
-    ?>
-        <li> <?= " Titolo: {$post_15_stampa['title']} , autore: {$post_15_stampa['author']} e testo : {$post_15_stampa['text']} .  " ?> </li>
-    <? }
-    ?>
-</ul>
+    </ul>
+<?
+}
+?>
